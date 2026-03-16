72665c578629e01db6d8d304a4832665de677f74
Kelyan <advertise@kelyanrodiergilson.fr>
2026-03-16 18:12:02 +0100

    fix final injection supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
